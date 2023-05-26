1898297367f67c29b23eb0760c70def66dda74b5
Filipe Barros <tiagosoarez6@gmail.com>
2023-05-26 16:01:41 +0100

    first commit

---
+++ src/main/java/com/example/projeto2springwebmvc/Projeto2SpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto2springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2SpringWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projeto2SpringWebMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projeto2springwebmvc/Projeto2SpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto2springwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto2SpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
